1afcd075610c6301c9904fd9d9d499cf5208d625
Tim Foley <thetimfoley@gmail.com>
2016-10-26 19:13:15 -0400

    reading from API. Adding Create now.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,58 @@
 "use strict";
 
 (function(){
-  angular.module("wdinstagram", [])
+  angular.module("wdinstagram", [
+    "ngResource",
+    "ui.router"
+  ])
+  .config([
+    "$stateProvider",
+    RouterFunction
+  ])
+  .controller("GramIndexController", [
+    "GramFactory",
+    GramIndexControllerFunction
+  ])
+  .controller("GramShowController", [
+    "GramFactory",
+    "$stateParams",
+    GramShowControllerFunction
+  ])
+  .factory("GramFactory", [
+    "$resource",
+    GramFactoryFunction
+  ])
+
+
+
+
+  function RouterFunction($stateProvider){
+    $stateProvider
+    .state("gramIndex", {
+      url: "/",
+      templateUrl: "js/ng-views/index.html",
+      controller: "GramIndexController",
+      controllerAs: "vm"
+    })
+    .state("gramShow", {
+      url: "/photos/:id",
+      templateUrl: "js/ng-views/show.html",
+      controller: "GramShowController",
+      controllerAs: "vm"
+    })
+
+  }
+
+  function GramIndexControllerFunction( GramFactory ){
+    this.grams = GramFactory.query();
+  }
+
+  function GramShowControllerFunction( GramFactory, $stateParams ){
+    this.gram = GramFactory.get({id: $stateParams.id})
+  }
+
+  function GramFactoryFunction( $resource ){
+    return $resource( "http://localhost:3000/entries/:id" );
+  }
+
 })();
